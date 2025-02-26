f06ecb2b2e20e353eb5c803e6ccc75075edb6709
Giang <140945898+dauvangiang@users.noreply.github.com>
2025-02-26 16:19:59 +0700

    import folder

---
+++ Apps/HelloToast/app/src/main/java/com/dvgiang/hellotoast/MainActivity.java
@@ -0,0 +1,45 @@
+package com.dvgiang.hellotoast;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+    private TextView showCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        showCount = findViewById(R.id.show_count);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(
+                this, R.string.toast_message,
+                Toast.LENGTH_LONG
+        );
+        toast.show();
+    }
+
+    public void increaseCount(View view) {
+        count ++;
+        if (showCount != null) {
+            showCount.setText(Integer.toString(count));
+        }
+    }
+}
